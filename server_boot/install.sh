c5ba2285346282489783dbff61aabd0a735fc198
plbbzd <yuntao_liu.sgmext@saic-gm.com>
2022-02-15 16:24:25 +0800

    更改位置

--- server_boot/install.sh
+++ server_boot/install.sh
@@ -8,9 +8,11 @@ if !command -v git >/dev/null 2>&1; then
     apt install -y git
 fi
 
+mkdir -p /data/script
+
 # 创建server_boot目录
-git clone http://git.iliuyt.com/liuyt/script.git ~/.lyt_script
+git clone http://git.iliuyt.com/liuyt/script.git /data/script
 
-cd ~/.lyt_script
+cd /data/script
 
-sh  ~/.lyt_script/server_boot/install_boot.sh
+sh  /data/script/server_boot/install_boot.sh
--- server_boot/install_boot.sh
+++ server_boot/install_boot.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # 更改apt源
-sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
-sudo cp ~/.lyt_script/lyt_script/ubuntu/20.04/source.list /etc/apt/sources.list
+mv /etc/apt/sources.list /etc/apt/sources.list.bak
+cp /data/script/server_boot/ubuntu/20.04/source.list /etc/apt/sources.list
 
 # 更新apt
 apt update
